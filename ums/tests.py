4249b4fde14a7eab823df16651445ad3a97885ef
PURAN123 <pchandra1002@gmail.com>
2022-04-08 15:45:41 +0530

    "

--- ums/tests.py
+++ ums/tests.py
@@ -1,11 +1,9 @@
 
-
 from django.contrib.auth.models import Group
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase
-from setuptools import setup
 
 from ums.models import School, User
 
